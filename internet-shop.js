43f9192db9b254fc8e9f0c96b587215aa0ea8240
Alzeth <mr.alzeth@gmail.com>
2017-02-28 15:59:30 +0200

    added static property and static method for Product

--- internet-shop.js
+++ internet-shop.js
@@ -3,8 +3,17 @@ Product = function (name, price, producer){
     this.name = name;
     this.price = price;
     this.producer = producer;
-    return this;
+    this.products = [ShirtLarge, PantsSmall, PillowCase, BedSheet, Cup, Plate];
+    this.filtered = filtered;
+    Product.search = function search() {
+      var arrstr = products.toString();
+      filtered = arrstr.match(/prompt('What do you want to find?')/g);
+      console.log(filtered);
+    }
+
   }
+
+
 Product.prototype.setName = function(){
   console.log(this.name + " setName");
 }
@@ -30,7 +39,6 @@ Cloth = function (name, price, producer, size, print) {
   Product.apply(this, arguments);
   this.size = size;
   this.print = print;
-  return this;
 }
 Cloth.prototype = Object.create(Product.prototype);
 Cloth.prototype.constructor = Cloth;
@@ -60,7 +68,6 @@ Linens = function (name, price, producer, color, size, material) {
   this.color = color;
   this.size = size;
   this.material = material;
-  return this;
 }
 Linens.prototype = Object.create(Product.prototype);
 Linens.constructor = Linens;
@@ -86,7 +93,6 @@ Dishes = function (name, price, producer, color, material) {
   Product.apply(this, arguments);
   this.color = color;
   this.material = material;
-  return this;
 }
 Dishes.prototype = Object.create(Product.prototype);
 Dishes.constructor = Dishes;
