d602300c5c0237fab5b1e03e34c5db9de6040c26
Alzeth <mr.alzeth@gmail.com>
2017-03-20 23:16:48 +0200

    added paginator, created few new products, modified css

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Code goes here
+
 //global prototype Product
 Product = function (name, price, producer){
     this.name = name;
@@ -25,6 +27,27 @@ var Cloth = function (name, price, producer, size, print) {
     this.size = size;
     this.print = print;
 };
+var Linens = function (name, price, producer, color, size, material) {
+    Product.apply(this, arguments);
+    this.color = color;
+    this.size = size;
+    this.material = material;
+};
+var Dishes = function (name, price, producer, color, material) {
+    Product.apply(this, arguments);
+    this.color = color;
+    this.material = material;
+};
+Dishes.create = (name, price, producer, color, material) => {
+    var product3 = new Dishes(name, price, producer);
+    Product.products.push(product3);
+    return product3;
+}
+Linens.create = (name, price, producer, color, size, material) => {
+    var product2 = new Linens(name, price, producer);
+    Product.products.push(product2);
+    return product2;
+}
 Cloth.create = (name, price, producer, size, print) => {
     var product = new Cloth(name, price, producer);
     Product.products.push(product);
@@ -44,7 +67,7 @@ Product.showProducts = function() {
         })
     })
 }
- //config with html template
+//config with html template
 Product.config = {
     templates: {
         productCard: `
@@ -60,7 +83,10 @@ Product.config = {
 Cloth.create("Shirt", "$5", "Versace", "L", "SixEyes");
 Cloth.create("Pants", "$10", "D&G", "S", "Logo");
 Cloth.create("Shoes", "$15", "Fallen", "9", "Fallen");
-
+Linens.create("Pillowcase", "$6", "Ottavapillows", "white", "50sm*70sm", "cotton");
+Linens.create("Bedsheet", "$12", "Torontolinens", "beige", "Standart", "cotton");
+Dishes.create("Cup", "$3", "Chineseporcelain", "red", "ceramics");
+Dishes.create("Plate", "$4", "Chineseporcelain", "seaShell", "porcelain");
 
 
 Product.prototype.setName = function(){
@@ -105,15 +131,10 @@ Cloth.prototype.setPrint = function(){
 };
 
 
-//
 
 
-Linens = function (name, price, producer, color, size, material) {
-    Product.apply(this, arguments);
-    this.color = color;
-    this.size = size;
-    this.material = material;
-};
+
+
 Linens.prototype = Object.create(Product.prototype);
 Linens.constructor = Linens;
 
@@ -133,11 +154,7 @@ Linens.prototype.setColor = function(){
 
 
 
-Dishes = function (name, price, producer, color, material) {
-    Product.apply(this, arguments);
-    this.color = color;
-    this.material = material;
-};
+
 Dishes.prototype = Object.create(Product.prototype);
 Dishes.constructor = Dishes;
 
@@ -151,21 +168,77 @@ Dishes.prototype.getName = function(){
     return this.name;
 };
 
+let btnContainer = document.querySelector(".btnContainer")
+function Paginator(data) {
+    this.data = data;
+}
+
+Paginator.config = {
+    perPage: 3,
+    totalPages: null,
+    currentPage: 1
+}
+
+Paginator.init = function() {
+    let prevBtn = document.createElement("button");
+    prevBtn.textContent = "<<<"
+    prevBtn.onclick = paginator.prev;
+    let nextBtn = document.createElement("button");
+    nextBtn.textContent = ">>>"
+    nextBtn.onclick = paginator.next;
+
+    /*    let buttons = document.createElement("span")
+     buttons.appendChild(prevBtn);
+     buttons.appendChild(nextBtn);*/
+    Paginator.refresh();
+    btnContainer.appendChild(prevBtn)
+    btnContainer.appendChild(nextBtn)
+}
+
+Paginator.refresh = () => {
+    let paginatedItems = paginator.paginate()
+    elem.innerHTML = ""
+    paginatedItems.forEach((item) => {
+        elem.insertAdjacentHTML("afterBegin", item)
+    });
+    console.log("currentPage", Paginator.config.currentPage)
+    //Paginator.init()
+}
+
+Paginator.prototype.paginate = function() {
+    let config = Paginator.config;
+    config.totalPages = Math.floor(this.data.length / config.perPage);
+    // console.log(this.data.length / config.perPage )
+    return this.data.filter((item, index) => {
+        //console.log("index=", index)
+        // вывод элементов между currentPage и currentPage * perPage не включая
+        if (index >= (config.currentPage-1)*config.perPage && index < (config.currentPage)*(config.perPage)) {
+            return item
+        }
+    })
+}
+
+Paginator.prototype.next = function() {
+    console.log("<currentPage", Paginator.config.currentPage)
+    Paginator.config.currentPage++;
+    Paginator.refresh();
+    console.log(">currentPage", Paginator.config.currentPage)
+}
+Paginator.prototype.prev = function() {
+    console.log("<currentPage", Paginator.config.currentPage)
+    Paginator.config.currentPage--;
+    Paginator.refresh();
+    console.log(">currentPage", Paginator.config.currentPage)
+}
 
 
 
 
 
 
-   /* var PillowCase = new Linens("Pillowcase", "$6", "Ottavapillows", "white", "50sm*70sm", "cotton");
-    Product.products.push(PillowCase);
-    var BedSheet = new Linens("Bedsheet", "$12", "Torontolinens", "beige", "Standart", "cotton");
-    Product.products.push(BedSheet);
-    var Cup = new Dishes("Cup", "$3", "Chineseporcelain", "red", "ceramics");
-    Product.products.push(Cup);
-    var Plate = new Dishes("Plate", "$4", "Chineseporcelain", "seaShell", "porcelain");
-    Product.products.push(Plate);*/
 var elem = document.getElementById('products');
 let products = Product.showProducts();
 elem.insertAdjacentHTML("afterBegin", products.join(" "));
+var paginator = new Paginator(products);
+Paginator.init()
 
